3003a1b8aa406111b154a5f7e01e746b03e6ec68
Niv Sardi <xaiki@evilgiggle.com>
2018-06-02 10:01:49 -0300

    ES6 class
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,80 +1,76 @@
-var inherits  = require('util').inherits
-    , request = require('request')
-    , debug   = require('debug')('butter-streamer-http');
+const request = require('request')
+const debug = require('debug')('butter-streamer-http')
 
-var Streamer = require('butter-base-streamer');
+const Streamer = require('butter-base-streamer')
+const config = {
+  name: 'HTTP Streamer',
+  protocol: /https?/,
+  type: 'http',
+  priority: 100
+}
 
 /* -- HTTP Streamer -- */
-function HttpStreamer(source, options) {
-	if(!(this instanceof HttpStreamer))
-		return new HttpStreamer(source, options);
+class HttpStreamer extends Streamer {
+  constructor (source, options) {
+    super(options)
+    options = options || {}
 
-	Streamer.call(this, options);
-	var self = this;
-	options = options || {};
+    this.request = request.defaults({
+      encoding: null
+    })
 
-	this.request = request.defaults({
-		encoding: null
-	});
+    this.config = config
 
-	this._options = options;
-	this._source = source;
-	this._req = this.request(source, options.http);
-	this._req.on('response', function(res) {
-		var length = self._req.getHeader('content-length', res.headers);
-		debug('got response', length)
+    this._options = options
+    this._source = source
+    this._req = this.request(source, options.http)
+    this._req.on('response', (res) => {
+      var length = this._req.getHeader('content-length', res.headers)
+      debug('got response', length)
 
-		if(length !== undefined) {
-			self._progress.setLength(parseInt(length));
-		}
+      if (length !== undefined) {
+        this._progress.setLength(parseInt(length))
+      }
 
-		self._isReady({
-			length: length,
-		});
-	})
-	this._streamify.resolve(this._req);
-}
-inherits(HttpStreamer, Streamer);
+      this._isReady({
+        length: length
+      })
+    })
+    this._streamify.resolve(this._req)
+  }
 
-HttpStreamer.prototype.config = {
-	name: 'HTTP Streamer',
-	protocol: /https?/,
-	type: 'http',
-	priority: 100
-}
+  seek (start, end) {
+    if (this._destroyed) throw new ReferenceError('Streamer already destroyed')
 
-HttpStreamer.prototype.seek = function(start, end) {
-	if(this._destroyed) throw new ReferenceError('Streamer already destroyed');
+    var self = this
+    start = start || 0
 
-	var self = this;
-	start = start || 0;
+    if (this._req) { this._req.destroy() }
 
-	if(this._req)
-		this._req.destroy();
+    this._req = this.request(this._source, {
+      headers: {
+        'Range': 'bytes=' + start + '-' + (end !== undefined ? end : '')
+      }
+    }).on('response', function (res) {
+      var length = self._req.getHeader('content-length', res.headers)
+      if (length !== undefined) { self._progress.setLength(parseInt(length)) }
+    })
 
-	this._req = this.request(this._source, {
-		headers: {
-			'Range': 'bytes=' + start + '-' + (end !== undefined ? end : '')
-		}
-	}).on('response', function(res) {
-		var length = self._req.getHeader('content-length', res.headers);
-		if(length !== undefined)
-			self._progress.setLength(parseInt(length));
-	})
+    this._streamify.unresolve()
+    this._streamify.resolve(this._req)
+  }
 
-	this._streamify.unresolve();
-	this._streamify.resolve(this._req);
-}
+  destroy () {
+    if (this._destroyed) throw new ReferenceError('Streamer already destroyed')
 
-HttpStreamer.prototype.destroy = function() {
-	if(this._destroyed) throw new ReferenceError('Streamer already destroyed');
-
-	if(this._req)
-		this._req.destroy();
-	this._streamify.unresolve();
-	this._req = null;
-	this._destroyed = true;
-	this.file = {};
+    if (this._req) { this._req.destroy() }
+    this._streamify.unresolve()
+    this._req = null
+    this._destroyed = true
+    this.file = {}
+  }
 }
 
-module.exports = HttpStreamer;
+HttpStreamer.config = config
+
+module.exports = HttpStreamer
