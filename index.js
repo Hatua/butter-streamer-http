df5075706098acc293c2f0b8950eb72d60b0d946
Niv Sardi <xaiki@evilgiggle.com>
2018-06-03 01:05:13 -0300

    move to 0.1.0 API
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const debug = require('debug')('butter-streamer-http')
 
-const Streamer = require('butter-base-streamer')
+const Streamer = require('butter-streamer')
 const config = {
   name: 'HTTP Streamer',
   protocol: /https?/,
@@ -11,9 +11,8 @@ const config = {
 
 /* -- HTTP Streamer -- */
 class HttpStreamer extends Streamer {
-  constructor (source, options) {
+  constructor (source, options = {}) {
     super(options)
-    options = options || {}
 
     this.request = request.defaults({
       encoding: null
@@ -23,26 +22,22 @@ class HttpStreamer extends Streamer {
 
     this._options = options
     this._source = source
+
     this._req = this.request(source, options.http)
     this._req.on('response', (res) => {
-      var length = this._req.getHeader('content-length', res.headers)
+      const length = Number(res.headers['content-length'])
       debug('got response', length)
 
-      if (length !== undefined) {
-        this._progress.setLength(parseInt(length))
+      if (length) {
+        this.ready(this._req, {length,})
       }
-
-      this._isReady({
-        length: length
-      })
     })
-    this._streamify.resolve(this._req)
   }
 
   seek (start, end) {
     if (this._destroyed) throw new ReferenceError('Streamer already destroyed')
 
-    var self = this
+    const self = this
     start = start || 0
 
     if (this._req) { this._req.destroy() }
@@ -52,19 +47,16 @@ class HttpStreamer extends Streamer {
         'Range': 'bytes=' + start + '-' + (end !== undefined ? end : '')
       }
     }).on('response', function (res) {
-      var length = self._req.getHeader('content-length', res.headers)
-      if (length !== undefined) { self._progress.setLength(parseInt(length)) }
+      const length = Number(res.headers['content-length'])
+      this.reset(this._req, {length,})
     })
-
-    this._streamify.unresolve()
-    this._streamify.resolve(this._req)
   }
 
   destroy () {
     if (this._destroyed) throw new ReferenceError('Streamer already destroyed')
 
     if (this._req) { this._req.destroy() }
-    this._streamify.unresolve()
+    this.close()
     this._req = null
     this._destroyed = true
     this.file = {}
